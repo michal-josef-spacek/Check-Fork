561bcce282adab17b08cd9064f49b9a3ef7c83fc
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2021-10-13 16:11:54 +0200

    Fix check_fork()

--- Fork.pm
+++ Fork.pm
@@ -13,23 +13,23 @@ Readonly::Array our @EXPORT_OK => qw(check_fork $ERROR_MESSAGE);
 our $VERSION = 0.01;
 
 sub check_fork {
-	if (! $Config{'d_fork'}) {
-		$ERROR_MESSAGE = 'No fork() routine available.';
-		return 0;
-	}
 
-	if (($^O eq 'MSWin32' || $^O eq 'NetWare')) {
-		if (! $Config{'useithreads'}) {
-			$ERROR_MESSAGE = "$^O: No interpreter-based threading implementation.";
+	if ($config_hr->{'d_fork'}) {
+		return 1;
+	} elsif ($os eq 'MSWin32' || $os eq 'NetWare') {
+		if (! $config_hr->{'useithreads'}) {
+			$ERROR_MESSAGE = "$os: No interpreter-based threading implementation.";
 			return 0;
 		}
-		if ($Config{'ccflags'} !~ /-DPERL_IMPLICIT_SYS/) {
-			$ERROR_MESSAGE = "$^O: No PERL_IMPLICIT_SYS ccflags set.";
+		if ($config_hr->{'ccflags'} !~ /-DPERL_IMPLICIT_SYS/) {
+			$ERROR_MESSAGE = "$os: No PERL_IMPLICIT_SYS ccflags set.";
 			return 0;
 		}
+		return 1;
 	}
+	$ERROR_MESSAGE = 'No fork() routine available.';
 
-	return 1;
+	return 0;
 }
 
 1;
