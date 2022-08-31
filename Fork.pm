5ca722219b34ae336bdbbff59744c873793c1ce6
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-08-31 20:07:56 +0200

    Add example to doc for example test which use fork and socket functionality

--- Fork.pm
+++ Fork.pm
@@ -140,6 +140,28 @@ Returns 0/1.
  # Output on Unix:
  # We could use fork and socket communication.
 
+=head1 EXAMPLE3
+
+=for comment filename=test_fork_and_socket.pl
+
+ use strict;
+ use warnings;
+
+ use Check::Fork qw(check_fork);
+ use Check::Socket qw(check_socket);
+ use Test::More 'tests' => 1;
+
+ SKIP: {
+         skip $Check::Fork::ERROR_MESSAGE, 1 unless check_fork();
+         skip $Check::Socket::ERROR_MESSAGE, 1 unless check_socket();
+
+         ok(1, 'Fork and Socket test');
+ };
+
+ # Output on Unix:
+ # 1..1
+ # ok 1 - Fork and Socket test
+
 =head1 DEPENDENCIES
 
 L<Config>,
